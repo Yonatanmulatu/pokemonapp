609ec20ee3047559667eb1db48f95e026b0743ff
Yonatanmulatu <yonatan.t.mulatu@gmail.com>
2020-08-06 14:22:55 +0800

    Updates

---
+++ precache-manifest.40e06d3bf5a14de8949369367c3c5acc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "463b0e6bdc1e264ceb424c8891bb02aa",
+    "url": "/pokemonapp/index.html"
+  },
+  {
+    "revision": "1eb90382ea28ce5bb7c2",
+    "url": "/pokemonapp/static/css/main.aad9a8ba.chunk.css"
+  },
+  {
+    "revision": "ba5f2edd7c26d07d22ca",
+    "url": "/pokemonapp/static/js/2.738ccd38.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/pokemonapp/static/js/2.738ccd38.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1eb90382ea28ce5bb7c2",
+    "url": "/pokemonapp/static/js/main.d6bf1ad8.chunk.js"
+  },
+  {
+    "revision": "d8e1555b16a0e3431fe7",
+    "url": "/pokemonapp/static/js/runtime-main.19a3eec6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/pokemonapp/static/media/logo.5d5d9eef.svg"
+  }
+]);
